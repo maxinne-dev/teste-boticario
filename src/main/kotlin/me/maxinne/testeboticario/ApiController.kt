92782dcf2f19b464b2dc6ead933afe5971de04df
Maxinne Schreiner <hi@maxinne.me>
2024-06-18 16:18:37 -0300

    Regras simples de Cache

--- src/main/kotlin/me/maxinne/testeboticario/ApiController.kt
+++ src/main/kotlin/me/maxinne/testeboticario/ApiController.kt
@@ -1,9 +1,11 @@
 package me.maxinne.testeboticario
 
+import org.springframework.http.CacheControl
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RestController
+import java.util.concurrent.TimeUnit
 
 @RestController
 class ApiController(val dataSource: DummyDataSource) {
@@ -11,14 +13,22 @@ class ApiController(val dataSource: DummyDataSource) {
     fun healthcheck(): String {
         return "ok"
     }
+
     @GetMapping(value = ["/essences"])
     fun essences(): ResponseEntity<List<DummyShortData>> {
-        return ResponseEntity.ok(dataSource.getData())
+        return ResponseEntity
+            .ok()
+            .cacheControl(CacheControl.maxAge(30, TimeUnit.MINUTES).mustRevalidate())
+            .body(dataSource.getData())
     }
+
     @GetMapping(value = ["/essences/{essence-id}"])
     fun essence(@PathVariable("essence-id") essenceId: String): ResponseEntity<DummyData> {
         return try {
-            ResponseEntity.ok(dataSource.getDataById(essenceId))
+            ResponseEntity
+                .ok()
+                .cacheControl(CacheControl.maxAge(30, TimeUnit.MINUTES).mustRevalidate())
+                .body(dataSource.getDataById(essenceId))
         } catch (e: Exception) {
             ResponseEntity.notFound().build()
         }
